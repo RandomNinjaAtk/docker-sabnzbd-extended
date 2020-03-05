5f74fa4033a87970b3eb73ce9aaec82e4116a17c
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-04 20:08:39 -0500

    Update 02-script-setup.bash

--- root/etc/cont-init.d/02-script-setup.bash
+++ root/etc/cont-init.d/02-script-setup.bash
@@ -2,7 +2,7 @@
 
 # Create scripts directory
 if [ ! -d "/config/scripts" ]; then
-	mkdir "/config/scripts"
+	mkdir -p "/config/scripts"
 fi
 
 # Remove existing LAD start script
@@ -16,5 +16,21 @@ if [ ! -f "/config/scripts/AudioPostProcessing.bash" ]; then
 	cp "/root/scripts/AudioPostProcessing.bash" "/config/scripts/AudioPostProcessing.bash"
 	chmod 0777 "/config/scripts/AudioPostProcessing.bash"
 fi
+
+# Create downloads incomplete directory
+if [ ! -d "/downloads/incomplete" ]; then
+	mkdir -p "/downloads/incomplete"
+	chmod 0777 "/downloads/incomplete"
+fi
+
+# Create downloads complete directory
+if [ ! -d "/downloads/complete" ]; then
+	mkdir -p "/downloads/complete"
+	chmod 0777 "/downloads/incomplete"
+fi
+
+sed -i "s/script_dir = \"\"/script_dir = \"/config\/scripts\"/g" "/config/sabnzbd.ini" && \
+sed -i "s/Downloads\/incomplete/\/downloads\/incomplete/g" "/config/sabnzbd.ini" && \
+sed -i "s/Downloads\/complete/\/downloads\/complete/g" "/config/sabnzbd.ini"
 
 exit 0
