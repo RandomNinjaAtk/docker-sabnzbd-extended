fa72c7f95befc72cab4a540f021b7828f5e228c6
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-04 21:18:29 -0500

    Update 02-script-setup.bash

--- root/etc/cont-init.d/02-script-setup.bash
+++ root/etc/cont-init.d/02-script-setup.bash
@@ -29,8 +29,36 @@ if [ ! -d "/downloads/complete" ]; then
 	chmod 0777 "/downloads/incomplete"
 fi
 
-sed -i "s/script_dir = \"\"/script_dir = \"\/config\/scripts\"/g" "/config/sabnzbd.ini" && \
-sed -i "s/Downloads\/incomplete/\/downloads\/incomplete/g" "/config/sabnzbd.ini" && \
+sed -i "s/script_dir = \"\"/script_dir = \"\/config\/scripts\"/g" "/config/sabnzbd.ini"
+sed -i "s/Downloads\/incomplete/\/downloads\/incomplete/g" "/config/sabnzbd.ini"
 sed -i "s/Downloads\/complete/\/downloads\/complete/g" "/config/sabnzbd.ini"
+head -3 "/config/sabnzbd.ini"
+
+echo "[[radarr]]" >> "/config/sabnzbd.ini"
+echo "priority = -100" >> "/config/sabnzbd.ini"
+echo "pp = \"\"" >> "/config/sabnzbd.ini"
+echo "name = radarr" >> "/config/sabnzbd.ini"
+echo "script = Default" >> "/config/sabnzbd.ini"
+echo "newzbin = \"\"" >> "/config/sabnzbd.ini"
+echo "order = 0" >> "/config/sabnzbd.ini"
+echo "dir = radarr" >> "/config/sabnzbd.ini"
+
+echo "[[sonarr]]" >> "/config/sabnzbd.ini"
+echo "priority = -100" >> "/config/sabnzbd.ini"
+echo "pp = \"\"" >> "/config/sabnzbd.ini"
+echo "name = sonarr" >> "/config/sabnzbd.ini"
+echo "script = Default" >> "/config/sabnzbd.ini"
+echo "newzbin = \"\"" >> "/config/sabnzbd.ini"
+echo "order = 0" >> "/config/sabnzbd.ini"
+echo "dir = sonarr" >> "/config/sabnzbd.ini"
+
+echo "[[lidarr]]" >> "/config/sabnzbd.ini"
+echo "priority = -100" >> "/config/sabnzbd.ini"
+echo "pp = \"\"" >> "/config/sabnzbd.ini"
+echo "name = lidarr" >> "/config/sabnzbd.ini"
+echo "script = AudioPostProcessing.bash" >> "/config/sabnzbd.ini"
+echo "newzbin = \"\"" >> "/config/sabnzbd.ini"
+echo "order = 0" >> "/config/sabnzbd.ini"
+echo "dir = lidarr" >> "/config/sabnzbd.ini"
 
 exit 0
