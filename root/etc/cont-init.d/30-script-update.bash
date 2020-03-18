312dc405eb767ce51c9c924586b5ef61c7a83411
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-18 17:54:48 -0400

    Update 30-script-update.bash

--- root/etc/cont-init.d/30-script-update.bash
+++ root/etc/cont-init.d/30-script-update.bash
@@ -1,9 +1,13 @@
 #!/usr/bin/with-contenv bash
 
 # update from git
-if [[ "${UPDATE}" == "true" ]]; then
+if [[ "${UPDATE_EXT}" == "TRUE" ]]; then
     git -C ${SABSCRIPTS_PATH} reset --hard HEAD && \
     git -C ${SABSCRIPTS_PATH} pull origin master && \
+fi
+
+# update from git
+if [[ "${UDPATE_SMA}" == "TRUE" ]]; then
     git -C ${SMA_PATH} reset --hard HEAD && \
     git -C ${SMA_PATH} pull origin master
 fi
