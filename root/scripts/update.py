e269e8ec60355cba30891b66a018f23942bcce1d
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-01 19:24:27 -0400

    bugfix

--- root/scripts/update.py
+++ root/scripts/update.py
@@ -20,10 +20,12 @@ def main():
         safeConfigParser.set("Converter", "threads", os.environ.get("CONVERTER_THREADS"))
     if os.environ.get("CONVERTER_HWACCELS"):
         safeConfigParser.set("Converter", "hwaccels", os.environ.get("CONVERTER_HWACCELS"))
+    else:
+        safeConfigParser.set("Converter", "hwaccels", " ")
     if os.environ.get("CONVERTER_HWACCEL_DECODERS"):
         safeConfigParser.set("Converter", "hwaccel-decoders", os.environ.get("CONVERTER_HWACCEL_DECODERS"))
-    if os.environ.get("CONVERTER_THREADS"):
-        safeConfigParser.set("Converter", "threads", os.environ.get("CONVERTER_THREADS"))
+    else:
+        safeConfigParser.set("Converter", "hwaccel-decoders", " ")
     if os.environ.get("CONVERTER_OUTPUT_FORMAT"):    
         safeConfigParser.set("Converter", "output-format", os.environ.get("CONVERTER_OUTPUT_FORMAT"))
     if os.environ.get("CONVERTER_OUTPUT_EXTENSION"):    
