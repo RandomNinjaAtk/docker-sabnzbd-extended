ca1df4ba4ccc7a318b5ccd97de8fbb272df0180a
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-02 15:21:24 -0400

    bugfix...

--- root/scripts/update.py
+++ root/scripts/update.py
@@ -13,7 +13,7 @@ def main():
     safeConfigParser = configparser.ConfigParser()
     safeConfigParser.read(autoProcess)
 
-    # Set FFMPEG/FFProbe Paths
+    # Set Converter Settings
     safeConfigParser.set("Converter", "ffmpeg", "ffmpeg")
     safeConfigParser.set("Converter", "ffprobe", "ffprobe")
     if os.environ.get("CONVERTER_THREADS"):
@@ -40,8 +40,12 @@ def main():
         safeConfigParser.set("Converter", "force-convert", os.environ.get("CONVERTER_FORCE_CONVERT"))
     if os.environ.get("CONVERTER_PREOPTS"):
         safeConfigParser.set("Converter", "preopts", os.environ.get("CONVERTER_PREOPTS"))
+    else:
+        safeConfigParser.set("Converter", "preopts", " ")
     if os.environ.get("CONVERTER_POSTOPTS"):    
         safeConfigParser.set("Converter", "postopts", os.environ.get("CONVERTER_POSTOPTS"))
+    else:
+        safeConfigParser.set("Converter", "postopts", " ")
     
     # SET Permissions
     if os.environ.get("PERMISSIONS_CHMOD"):
@@ -68,22 +72,32 @@ def main():
         safeConfigParser.set("Video", "crf", os.environ.get("VIDEO_CRF"))
     if os.environ.get("VIDEO_CRF_PROFILES"):
         safeConfigParser.set("Video", "crf-profiles", os.environ.get("VIDEO_CRF_PROFILES"))
+    else:
+        safeConfigParser.set("Video", "crf-profiles", " ")
     if os.environ.get("VIDEO_MAX_WIDTH"):
         safeConfigParser.set("Video", "max-width", os.environ.get("VIDEO_MAX_WIDTH"))
     if os.environ.get("VIDEO_PROFILE"):
         safeConfigParser.set("Video", "profile", os.environ.get("VIDEO_PROFILE"))
+    else:
+        safeConfigParser.set("Video", "profile", " ")
     if os.environ.get("VIDEO_MAX_LEVEL"):
         safeConfigParser.set("Video", "max-level", os.environ.get("VIDEO_MAX_LEVEL"))
     if os.environ.get("VIDEO_PIX_FMT"):
         safeConfigParser.set("Video", "pix-fmt", os.environ.get("VIDEO_PIX_FMT"))
+    else:
+        safeConfigParser.set("Video", "pix-fmt", " ")
     
     # Set Audio Settings
     if os.environ.get("AUDIO_CODEC"):
         safeConfigParser.set("Audio", "codec", os.environ.get("AUDIO_CODEC"))
     if os.environ.get("AUDIO_LANGUAGES"):
         safeConfigParser.set("Audio", "languages", os.environ.get("AUDIO_LANGUAGES"))
+    else:
+        safeConfigParser.set("Audio", "languages", " ")
     if os.environ.get("AUDIO_DEFAULT_LANGUAGE"):
         safeConfigParser.set("Audio", "default-language", os.environ.get("AUDIO_DEFAULT_LANGUAGE"))
+    else:
+        safeConfigParser.set("Audio", "default-language", " ")
     if os.environ.get("AUDIO_FIRST_STREAM_OF_LANGUAGE"):
         safeConfigParser.set("Audio", "first-stream-of-language", os.environ.get("AUDIO_FIRST_STREAM_OF_LANGUAGE"))
     if os.environ.get("AUDIO_CHANNEL_BITRATE"):
@@ -98,8 +112,12 @@ def main():
         safeConfigParser.set("Audio", "default-more-channels", os.environ.get("AUDIO_DEFAULT_MORE_CHANNELS"))
     if os.environ.get("AUDIO_FILTER"):
         safeConfigParser.set("Audio", "filter", os.environ.get("AUDIO_FILTER"))
+    else:
+        safeConfigParser.set("Audio", "filter", " ")
     if os.environ.get("AUDIO_SAMPLE_RATES"):
         safeConfigParser.set("Audio", "sample-rates", os.environ.get("AUDIO_SAMPLE_RATES"))
+    else:
+        safeConfigParser.set("Audio", "sample-rates", " ")
     if os.environ.get("AUDIO_COPY_ORIGINAL"):
         safeConfigParser.set("Audio", "copy-original", os.environ.get("AUDIO_COPY_ORIGINAL"))
     if os.environ.get("AUDIO_AAC_ADTSTOASC"):
@@ -110,6 +128,8 @@ def main():
     # Set Universal Audio Settings
     if os.environ.get("UAUDIO_CODEC"):
         safeConfigParser.set("Universal Audio", "codec", os.environ.get("UAUDIO_CODEC"))
+    else:
+        safeConfigParser.set("Universal Audio", "codec", " ")
     if os.environ.get("UAUDIO_CHANNEL_BITRATE"):
         safeConfigParser.set("Universal Audio", "channel-bitrate", os.environ.get("UAUDIO_CHANNEL_BITRATE"))
     if os.environ.get("UAUDIO_FIRST_STREAM_ONLY"):
@@ -118,30 +138,44 @@ def main():
         safeConfigParser.set("Universal Audio", "move-after", os.environ.get("UAUDIO_MOVE_AFTER"))
     if os.environ.get("UAUDIO_FILTER"):
         safeConfigParser.set("Universal Audio", "filter", os.environ.get("UAUDIO_FILTER"))
+    else:
+        safeConfigParser.set("Universal Audio", "filter", " ")
                          
     # Set Subtitle Settings
     if os.environ.get("SUBTITLE_CODEC"):
         safeConfigParser.set("Subtitle", "codec", os.environ.get("SUBTITLE_CODEC"))
     if os.environ.get("SUBTITLE_CODEC_IMAGE_BASED"):
         safeConfigParser.set("Subtitle", "codec-image-based", os.environ.get("SUBTITLE_CODEC_IMAGE_BASED"))
+    else:
+        safeConfigParser.set("Subtitle", "codec-image-based", " ")
     if os.environ.get("SUBTITLE_LANGUAGES"):
         safeConfigParser.set("Subtitle", "languages", os.environ.get("SUBTITLE_LANGUAGES"))
+    else:
+        safeConfigParser.set("Subtitle", "languages", " ")
     if os.environ.get("SUBTITLE_DEFAULT_LANGUAGE"):
         safeConfigParser.set("Subtitle", "default-language", os.environ.get("SUBTITLE_DEFAULT_LANGUAGE"))
+    else:
+        safeConfigParser.set("Subtitle", "default-language", " ")
     if os.environ.get("SUBTITLE_FIRST_STREAM_OF_LANGUAGE"):
         safeConfigParser.set("Subtitle", "first-stream-of-language", os.environ.get("SUBTITLE_FIRST_STREAM_OF_LANGUAGE"))
     if os.environ.get("SUBTITLE_ENCODING"):
         safeConfigParser.set("Subtitle", "encoding", os.environ.get("SUBTITLE_ENCODING"))
+    else:
+        safeConfigParser.set("Subtitle", "encoding", " ")
     if os.environ.get("SUBTITLE_BURN_SUBTITLES"):
         safeConfigParser.set("Subtitle", "burn-subtitles", os.environ.get("SUBTITLE_BURN_SUBTITLES"))
     if os.environ.get("SUBTITLE_BURN_DISPOSITIONS"):
         safeConfigParser.set("Subtitle", "burn-dispositions", os.environ.get("SUBTITLE_BURN_DISPOSITIONS"))
+    else:
+        safeConfigParser.set("Subtitle", "burn-dispositions", " ")
     if os.environ.get("SUBTITLE_DOWNLOAD_SUBS"):
         safeConfigParser.set("Subtitle", "download-subs", os.environ.get("SUBTITLE_DOWNLOAD_SUBS"))
     if os.environ.get("SUBTITLE_DOWNLOAD_HEARING_IMPAIRED_SUBS"):
         safeConfigParser.set("Subtitle", "download-hearing-impaired-subs", os.environ.get("SUBTITLE_DOWNLOAD_HEARING_IMPAIRED_SUBS"))
     if os.environ.get("SUBTITLE_DOWNLOAD_PROVIDERS"):
         safeConfigParser.set("Subtitle", "download-providers", os.environ.get("SUBTITLE_DOWNLOAD_PROVIDERS"))
+    else:
+        safeConfigParser.set("Subtitle", "download-providers", " ")
     if os.environ.get("SUBTITLE_EMBED_SUBS"):
         safeConfigParser.set("Subtitle", "embed-subs", os.environ.get("SUBTITLE_EMBED_SUBS"))
     if os.environ.get("SUBTITLE_EMBED_IMAGE_SUBS"):
@@ -154,6 +188,8 @@ def main():
         safeConfigParser.set("Subtitle", "ignore-embedded-subs", os.environ.get("SUBTITLE_IGNORE_EMBEDDED_SUBS"))
     if os.environ.get("SUBTITLE_ATTACHMENT_CODEC"):
         safeConfigParser.set("Subtitle", "attachment-codec", os.environ.get("SUBTITLE_ATTACHMENT_CODEC"))
+    else:
+        safeConfigParser.set("Subtitle", "attachment-codec", " ")
                          
     # Set Plex Settings
     if os.environ.get("PLEX_HOST"):
