752857dcc6e6efeaed3ddac7471921d1ad3257cb
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-09-08 07:12:51 -0400

    Update video-pp.bash

--- root/scripts/video-pp.bash
+++ root/scripts/video-pp.bash
@@ -316,7 +316,7 @@ find "$1" -type f -iregex ".*/.*\.\(mkv\|mp4\|avi\)" -print0 | while IFS= read -
 			echo "Begin processing with Sickbeard MP4 Automator..."
 			echo ""
 			# Manual run of Sickbeard MP4 Automator
-			if python3 /usr/local/sma/manual.py --config "$2" -i "${basefilename}.${extension}" $tagging; then
+			if python3 /usr/local/sma/manual.py --config "$5" -i "${basefilename}.${extension}" $tagging; then
 				echo "Processing complete for: ${filename}!"
 			else
 				echo "ERROR: Sickbeard MP4 Automator Processing Error"
