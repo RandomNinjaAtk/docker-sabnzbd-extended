26e720320d6017e8d47ca198602b382bd24e759e
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2022-07-23 13:20:01 -0400

    Update to new support URL

--- root/etc/cont-init.d/99-start.bash
+++ root/etc/cont-init.d/99-start.bash
@@ -1,11 +1,11 @@
-#!/usr/bin/with-contenv bash
-echo "------------------------------------------------------------"
+#!/usr/bin/env bash
+echo "------------------------------------------------------------------------------"
 echo "|~) _ ._  _| _ ._ _ |\ |o._  o _ |~|_|_|"
 echo "|~\(_|| |(_|(_)| | || \||| |_|(_||~| | |<"
 echo "Presenets: sabnzbd-extended"
-echo "------------------------------------------------------------"
+echo "------------------------------------------------------------------------------"
 echo "Donate: https://github.com/sponsors/RandomNinjaAtk"
 echo "Project: https://github.com/RandomNinjaAtk/docker-sabnzbd-extended"
-echo "Support: https://discord.gg/JumQXDc"
-echo "------------------------------------------------------------"
+echo "Support: https://github.com/RandomNinjaAtk/docker-sabnzbd-extended/discussions"
+echo "------------------------------------------------------------------------------"
 exit 0
