f633b879141cce8f07b89a711883243989411f3f
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2023-01-25 11:25:23 -0500

    v1.0.25 - Use functions for various processes
    Simplify code...

--- root/scripts/video-pp.bash
+++ root/scripts/video-pp.bash
@@ -2,7 +2,7 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 TITLESHORT="VPP"
-scriptVersion=1.0.24
+scriptVersion=1.0.25
 
 set -e
 set -o pipefail
@@ -52,6 +52,103 @@ function log {
     echo $m_time" :: $scriptVersion :: "$1
 }
 
+VideoLanguageCheck () {
+
+	count=0
+	fileCount=$(find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | wc -l)
+	log "Processing ${fileCount} video files..."
+	find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' file; do
+		count=$(($count+1))
+		baseFileName="${file%.*}"
+		fileName="$(basename "$file")"
+		extension="${fileName##*.}"
+		log "$count of $fileCount :: Processing $fileName"
+		videoData=$(ffprobe -v quiet -print_format json -show_streams "$file")
+		videoAudioTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .index" | wc -l)
+		videoSubtitleTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .index" | wc -l)
+		log "$count of $fileCount :: $videoAudioTracksCount Audio Tracks Found!"
+		log "$count of $fileCount :: $videoSubtitleTracksCount Subtitle Tracks Found!"
+		videoAudioLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .tags.language")
+		videoSubtitleLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .tags.language")
+
+		# Language Check
+		log "$count of $fileCount :: Checking for preferred languages \"$VIDEO_LANG\""
+		preferredLanguage=false
+		IFS=',' read -r -a filters <<< "$VIDEO_LANG"
+		for filter in "${filters[@]}"
+		do
+			videoAudioTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
+			videoSubtitleTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
+			log "$count of $fileCount :: $videoAudioTracksLanguageCount \"$filter\" Audio Tracks Found!"
+			log "$count of $fileCount :: $videoSubtitleTracksLanguageCount \"$filter\" Subtitle Tracks Found!"			
+			if [ "$preferredLanguage" == "false" ]; then
+				if echo "$videoAudioLanguages" | grep -i "$filter" | read; then
+					preferredLanguage=true
+				elif echo "$videoSubtitleLanguages" | grep -i "$filter" | read; then
+					preferredLanguage=true
+				fi
+			fi
+		done
+
+		if [ "$preferredLanguage" == "false" ]; then
+			if [ "$RequireLanguage" == "true" ]; then
+				log "$count of $fileCount :: ERROR :: No matching languages found in $(($videoAudioTracksCount + $videoSubtitleTracksCount)) Audio/Subtitle tracks"
+				log "$count of $fileCount :: ERROR :: Disable "
+				rm "$file" && log "INFO: deleted: $fileName"
+			fi
+		fi
+
+		
+		
+		log "$count of $fileCount :: Processing complete for: ${fileName}!"
+
+	done
+
+}
+
+VideoFileCheck () {
+	# check for video files
+	if find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | read; then
+		sleep 0.1
+	else
+		log "ERROR: No video files found for processing"
+		exit 1
+	fi
+}
+
+VideoSmaProcess (){
+	count=0
+	fileCount=$(find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | wc -l)
+	log "Processing ${fileCount} video files..."
+	find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' file; do
+		count=$(($count+1))
+		baseFileName="${file%.*}"
+		fileName="$(basename "$file")"
+		extension="${fileName##*.}"
+		log "$count of $fileCount :: Processing $fileName"
+		if [ -f "$file" ]; then	
+			if [ -f /usr/local/sma/config/sma.log ]; then
+				rm /usr/local/sma/config/sma.log
+			fi
+			log "$count of $fileCount :: Processing with SMA..."
+			if [ -f "/config/scripts/configs/$2-sma.ini" ]; then
+			
+			# Manual run of Sickbeard MP4 Automator
+				if python3 /usr/local/sma/manual.py --config "/config/scripts/configs/$2-sma.ini" -i "$file" $tagging; then
+					log "$count of $fileCount :: Complete!"
+				else
+					log "$count of $fileCount :: ERROR :: SMA Processing Error"
+					rm "$file" && log "INFO: deleted: $fileName"
+				fi
+			else
+				log "$count of $fileCount :: ERROR :: SMA Processing Error"
+				log "$count of $fileCount :: ERROR :: \"/config/scripts/configs/$2-sma.ini\" configuration file is missing..."
+				rm "$file" && log "INFO: deleted: $fileName"
+			fi
+		fi
+
+	done
+}
 
 function Main {
 	SECONDS=0
@@ -61,171 +158,16 @@ function Main {
 	category="$5"
 	
 	Configuration
+	VideoFileCheck "$folderpath"
+	VideoLanguageCheck "$folderpath"
+	VideoFileCheck "$folderpath"
+	VideoSmaProcess "$folderpath" "$category"
+	VideoFileCheck "$folderpath"
+	VideoLanguageCheck "$folderpath"	
+	VideoFileCheck "$folderpath"
 
-	# check for video files
-	if find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | read; then
-		sleep 0.1
-	else
-		log "ERROR: No video files found for processing"
-		exit 1
-	fi
-
-	count=0
-	fileCount=$(find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | wc -l)
-	log "Processing ${fileCount} video files..."
-	find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' file; do
-		count=$(($count+1))
-		baseFileName="${file%.*}"
-		fileName="$(basename "$file")"
-		extension="${fileName##*.}"
-		log "$count of $fileCount :: Processing $fileName"
-		videoData=$(ffprobe -v quiet -print_format json -show_streams "$file")
-		videoAudioTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .index" | wc -l)
-		videoSubtitleTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .index" | wc -l)
-		log "$count of $fileCount :: $videoAudioTracksCount Audio Tracks Found!"
-		log "$count of $fileCount :: $videoSubtitleTracksCount Subtitle Tracks Found!"
-		videoAudioLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .tags.language")
-		videoSubtitleLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .tags.language")
-
-		# Language Check
-		log "$count of $fileCount :: Checking for preferred languages \"$VIDEO_LANG\""
-		preferredLanguage=false
-		IFS=',' read -r -a filters <<< "$VIDEO_LANG"
-		for filter in "${filters[@]}"
-		do
-			videoAudioTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
-			videoSubtitleTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
-			log "$count of $fileCount :: $videoAudioTracksLanguageCount \"$filter\" Audio Tracks Found!"
-			log "$count of $fileCount :: $videoSubtitleTracksLanguageCount \"$filter\" Subtitle Tracks Found!"			
-			if [ "$preferredLanguage" == "false" ]; then
-				if echo "$videoAudioLanguages" | grep -i "$filter" | read; then
-					preferredLanguage=true
-				elif echo "$videoSubtitleLanguages" | grep -i "$filter" | read; then
-					preferredLanguage=true
-				fi
-			fi
-		done
-
-		if [ "$preferredLanguage" == "false" ]; then
-			if [ "$RequireLanguage" == "true" ]; then
-				log "$count of $fileCount :: ERROR :: No matching languages found in $(($videoAudioTracksCount + $videoSubtitleTracksCount)) Audio/Subtitle tracks"
-				log "$count of $fileCount :: ERROR :: Disable "
-				rm "$file" && log "INFO: deleted: $fileName"
-			fi
-		fi
-		
-		log "$count of $fileCount :: Processing complete for: ${fileName}!"
-	done
-
-	# check for video files
-	if find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | read; then
-		sleep 0.1
-	else
-		log "ERROR: No video files found for processing"
-		exit 1
-	fi
-
-	if [ ${VIDEO_SMA} = TRUE ]; then
-		count=0
-		fileCount=$(find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | wc -l)
-		log "Processing ${fileCount} video files..."
-		find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' file; do
-			count=$(($count+1))
-			baseFileName="${file%.*}"
-			fileName="$(basename "$file")"
-			extension="${fileName##*.}"
-			log "$count of $fileCount :: Processing $fileName"
-
-				if [ -f "$file" ]; then	
-					if [ -f /usr/local/sma/config/sma.log ]; then
-						rm /usr/local/sma/config/sma.log
-					fi
-
-					log "$count of $fileCount :: Processing with SMA..."
-					if [ -f "/config/scripts/configs/$5-sma.ini" ]; then
-						
-						# Manual run of Sickbeard MP4 Automator
-						if python3 /usr/local/sma/manual.py --config "/config/scripts/configs/$5-sma.ini" -i "$file" $tagging; then
-							log "$count of $fileCount :: Complete!"
-						else
-							log "$count of $fileCount :: ERROR :: SMA Processing Error"
-							rm "$file" && log "INFO: deleted: $fileName"
-						fi
-					else
-						log "$count of $fileCount :: ERROR :: SMA Processing Error"
-						log "$count of $fileCount :: ERROR :: \"/config/scripts/configs/$5-sma.ini\" configuration file is missing..."
-						rm "$file" && log "INFO: deleted: $fileName"
-					fi
-				fi
-
-		done
-	fi
-
-	# check for video files
-	if find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | read; then
-		sleep 0.1
-	else
-		log "ERROR: No video files found for processing"
-		exit 1
-	fi
-
-	count=0
-	fileCount=$(find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | wc -l)
-	log "Processing ${fileCount} video files..."
-	find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" -print0 | while IFS= read -r -d '' file; do
-		count=$(($count+1))
-		baseFileName="${file%.*}"
-		fileName="$(basename "$file")"
-		extension="${fileName##*.}"
-		log "$count of $fileCount :: Processing $fileName"
-		videoData=$(ffprobe -v quiet -print_format json -show_streams "$file")
-		videoAudioTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .index" | wc -l)
-		videoSubtitleTracksCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .index" | wc -l)
-		log "$count of $fileCount :: $videoAudioTracksCount Audio Tracks Found!"
-		log "$count of $fileCount :: $videoSubtitleTracksCount Subtitle Tracks Found!"
-		videoAudioLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | .tags.language")
-		videoSubtitleLanguages=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | .tags.language")
-
-		# Language Check
-		log "$count of $fileCount :: Checking for preferred languages \"$VIDEO_LANG\""
-		preferredLanguage=false
-		IFS=',' read -r -a filters <<< "$VIDEO_LANG"
-		for filter in "${filters[@]}"
-		do
-			videoAudioTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"audio\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
-			videoSubtitleTracksLanguageCount=$(echo "${videoData}" | jq -r ".streams[] | select(.codec_type==\"subtitle\") | select(.tags.language==\"${filter}\") | .index" | wc -l)
-			log "$count of $fileCount :: $videoAudioTracksLanguageCount \"$filter\" Audio Tracks Found!"
-			log "$count of $fileCount :: $videoSubtitleTracksLanguageCount \"$filter\" Subtitle Tracks Found!"			
-			if [ "$preferredLanguage" == "false" ]; then
-				if echo "$videoAudioLanguages" | grep -i "$filter" | read; then
-					preferredLanguage=true
-				elif echo "$videoSubtitleLanguages" | grep -i "$filter" | read; then
-					preferredLanguage=true
-				fi
-			fi
-		done
-
-		if [ "$preferredLanguage" == "false" ]; then
-			if [ "$RequireLanguage" == "true" ]; then
-				log "$count of $fileCount :: ERROR :: No matching languages found in $(($videoAudioTracksCount + $videoSubtitleTracksCount)) Audio/Subtitle tracks"
-				log "$count of $fileCount :: ERROR :: Disable "
-				rm "$file" && log "INFO: deleted: $fileName"
-			fi
-		fi
-
-		
-		
-		log "$count of $fileCount :: Processing complete for: ${fileName}!"
-
-	done
-		
-	if find "$1" -type f -regex ".*/.*\.\(m4v\|wmv\|mkv\|mp4\|avi\)" | read; then
-		duration=$SECONDS
-		echo "Post Processing Completed in $(($duration / 60 )) minutes and $(($duration % 60 )) seconds!"
-	else
-		log "ERROR :: Post Processing failed, no video files found..."
-		exit 1
-	fi
+	duration=$SECONDS
+	echo "Post Processing Completed in $(($duration / 60 )) minutes and $(($duration % 60 )) seconds!"
 }
 
 Main "$@" 
